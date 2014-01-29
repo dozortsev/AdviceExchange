5b5954bfd53dfbc962347873255ed823597252bb
Ant10 <dozortsevanton@gmail.com>
2014-01-29 22:25:07 +0200

    Add log trace to CRUD operations

--- dao/src/main/java/com/dozortsev/adviceexchange/dao/GenericDaoImpl.java
+++ dao/src/main/java/com/dozortsev/adviceexchange/dao/GenericDaoImpl.java
@@ -20,12 +20,11 @@ public class GenericDaoImpl<ID extends Serializable, T extends AbstractEntity<ID
 
     /*  todo: create question about log convention on crud method / @date - 29 Jan 14 12:22 AM */
 
-    @Override
-    public ID create(T entity) {
+    @Override public ID create(T entity) {
         try {
             log.info(format("Create new %s", getEntityClass()));
             getCurrentSession().save(entity);
-            log.info(format("Successful created"));
+            log.info(format("Successful created. ID: %s", entity.getId()));
 
         } catch (Exception e) {
             log.error("Error: ", e);
@@ -33,41 +32,52 @@ public class GenericDaoImpl<ID extends Serializable, T extends AbstractEntity<ID
         return entity.getId();
     }
 
-    @Override
-    public void delete(T entity) {
+    @Override public void delete(T entity) {
         try {
+            log.info(format("Deleting %s", getEntityClass()));
             getCurrentSession().delete(entity);
+            log.info("Successful deleted");
 
         } catch (Exception e) {
             log.error("Error: ", e);
         }
     }
 
-    @Override
-    public void delete(ID id) {
+    @Override public void delete(ID id) {
         try {
+            log.info(format("Deleting %s by Id: %s", getEntityClass(), id));
             T entity = (T) getCurrentSession().get(getEntityClass(), id);
 
-            getCurrentSession().delete(entity);
-
+            if (entity != null) {
+                getCurrentSession().delete(entity);
+                log.info("Successful deleted");
+                return;
+            }
+            log.info(format("%s with this ID not exist", getEntityClass()));
         } catch (Exception e) {
             log.error("Error: ", e);
         }
     }
 
-    @Override
-    public T get(ID id) {
+    @Override public T get(ID id) {
         try {
-            return (T) getCurrentSession().get(getEntityClass(), id);
+            log.info(format("Finding %s by Id: %s", getEntityClass(), id));
+            T entity = (T) getCurrentSession().get(getEntityClass(), id);
+
+            if (entity != null) {
+                log.info("Successful found");
+                return entity;
+            }
+            log.info("%s with this ID not exist");
         } catch (Exception e) {
             log.error("Error: ", e);
         }
         return null;
     }
 
-    @Override
-    public T update(T entity) {
+    @Override public T update(T entity) {
         try {
+            log.info("Updating %s. ID: %s", getEntityClass(), entity.getId());
             getCurrentSession().update(entity);
         } catch (Exception e) {
             log.error("Error: ", e);
