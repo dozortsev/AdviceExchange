d6033a546f7bc09c7f8aea97c0e564e334d50c9c
dozortsev <dozortsevanton@gmail.com>
2014-02-02 18:15:24 +0200

    Update mapping for AbstractEntity

--- domain/src/main/java/com/dozortsev/adviceexchange/domain/AbstractEntity.java
+++ domain/src/main/java/com/dozortsev/adviceexchange/domain/AbstractEntity.java
@@ -1,23 +1,19 @@
 package com.dozortsev.adviceexchange.domain;
 
-import org.hibernate.annotations.Generated;
-
-import javax.persistence.*;
-import javax.validation.constraints.NotNull;
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
 import static javax.persistence.GenerationType.IDENTITY;
-import static org.hibernate.annotations.GenerationTime.ALWAYS;
 
 @MappedSuperclass
 public abstract class AbstractEntity<ID extends Serializable> implements Serializable {
 
     @Id @GeneratedValue(strategy = IDENTITY)
-    @NotNull @Column(name = "id")
+    @Column(name = "id", unique = true, nullable = false)
     private ID id;
-
-    @Version @Generated(ALWAYS)
-    private Integer version;
 
     public ID getId() {
         return id;
@@ -25,27 +21,5 @@ public abstract class AbstractEntity<ID extends Serializable> implements Seriali
     @SuppressWarnings("unused")
     public void setId(ID id) {
         this.id = id;
-    }
-
-    public Integer getVersion() {
-        return version;
-    }
-    public void setVersion(Integer version) {
-        this.version = version;
-    }
-
-    @Override public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        AbstractEntity that = (AbstractEntity) o;
-
-        if (!id.equals(that.id)) return false;
-
-        return true;
-    }
-
-    @Override public int hashCode() {
-        return id.hashCode();
     }
 }
