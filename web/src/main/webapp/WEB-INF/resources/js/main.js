8ee029341af217775514e1c3acbf41fa5a5b68d7
dozortsev <dozortsevanton@gmail.com>
2014-04-07 23:50:48 +0300

    [#30]: login signup mockups

--- web/src/main/webapp/WEB-INF/resources/js/main.js
+++ web/src/main/webapp/WEB-INF/resources/js/main.js
@@ -1,3 +1,53 @@
 $('.ui.checkbox')
     .checkbox()
 ;
+
+
+/* error message on login */
+$('.message .close').on('click', function() {
+    $(this).closest('.message').fadeOut();
+});
+
+
+/* tabs */
+$(document).ready(function(){
+    $('.demo.menu .item').tab({history:false});
+});
+
+
+/* Form validation */
+$('.ui.form')
+    .form({
+        name: {
+            identifier  : 'name',
+            rules: [
+                {
+                    type   : 'empty',
+                    prompt : 'Please enter your user name'
+                }
+            ]
+        },
+        email: {
+            identifier : 'email',
+            rules: [
+                {
+                    type   : 'empty',
+                    prompt : 'Please enter a email'
+                }
+            ]
+        },
+        password: {
+            identifier : 'password',
+            rules: [
+                {
+                    type   : 'empty',
+                    prompt : 'Please enter a password'
+                },
+                {
+                    type   : 'length[6]',
+                    prompt : 'Your password must be at least 6 characters'
+                }
+            ]
+        }
+    })
+;
