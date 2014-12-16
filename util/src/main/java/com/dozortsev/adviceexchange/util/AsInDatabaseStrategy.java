87e6410879852f9bbee5d315855353b8d39390bf
dozortsev <dozortsevanton@gmail.com>
2014-12-16 20:11:31 +0200

    Delete jOOQ POJO generation

--- util/src/main/java/com/dozortsev/adviceexchange/util/AsInDatabaseStrategy.java
+++ util/src/main/java/com/dozortsev/adviceexchange/util/AsInDatabaseStrategy.java
@@ -4,24 +4,9 @@ import org.jooq.util.DefaultGeneratorStrategy;
 import org.jooq.util.Definition;
 import org.jooq.util.mysql.MySQLTableDefinition;
 
-import java.util.List;
-
-import static java.util.Arrays.asList;
 import static org.jooq.util.GeneratorStrategy.Mode.DEFAULT;
-import static org.jooq.util.GeneratorStrategy.Mode.POJO;
 
 public class AsInDatabaseStrategy extends DefaultGeneratorStrategy {
-
-    private static final List<String> userActivities = asList("answer", "question", "comment");
-
-    @Override public String getJavaClassExtends(Definition definition, Mode mode) {
-        if (POJO.equals(mode)) {
-            if (userActivities.contains(definition.getName())) {
-                return "UserActivity";
-            }
-        }
-        return Object.class.getName();
-    }
 
     @Override public String getJavaClassName(Definition definition, Mode mode) {
         String name = super.getJavaClassName(definition, mode);
